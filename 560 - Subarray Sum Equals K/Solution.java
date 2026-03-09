26823a3ab2483ebc3b82c1a4e8a8f2bd4500812d
Chitranshu <chitranshu2411@gmail.com>
2026-03-09 23:01:39 +0530

    Runtime : 29ms ( Beats 29.18% )

---
+++ 560 - Subarray Sum Equals K/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int result = 0;
+        int cumSum = 0;
+
+        mp.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            cumSum += nums[i];
+
+            if (mp.containsKey(cumSum - k)) {
+                result += mp.get(cumSum - k);
+            }
+
+            mp.put(cumSum, mp.getOrDefault(cumSum, 0) + 1);
+        }
+
+        return result;
+    }
+}
