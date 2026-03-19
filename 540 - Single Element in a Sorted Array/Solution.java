cd5076ccb308d86649a39eef56600a464f80356f
Chitranshu <chitranshu2411@gmail.com>
2026-03-19 23:30:48 +0530

    Runtime: 0ms ( Beats 100% )

---
+++ 540 - Single Element in a Sorted Array/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        int mid;
+
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            boolean isEven = (r - mid) % 2 == 0;
+
+            if (nums[mid] == nums[mid + 1]) {
+                if (isEven) {
+                    l = mid + 2;
+                } else {
+                    r = mid - 1;
+                }
+            } else if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                if (isEven) {
+                    r = mid - 2;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[l];
+    }
+}
