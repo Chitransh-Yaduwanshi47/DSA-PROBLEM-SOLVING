34a759c43328125a6452d19f04cbc1d9a4382f72
Chitranshu <chitranshu2411@gmail.com>
2026-04-07 17:17:25 +0530

    Time Taken: 0.14ms ( Accuracy 100% )

---
+++ GFG - Sorted Array Search/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+        int l = 0, r = arr.length-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] == k ){
+                return true;
+            } else if(arr[mid]<k){
+                l = mid + 1;
+            } else{
+                r = mid-1;
+            }
+        }
+        return false;
+    }
+}
