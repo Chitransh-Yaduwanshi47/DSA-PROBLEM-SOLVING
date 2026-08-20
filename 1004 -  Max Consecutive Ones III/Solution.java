2bd34bfbc76bbda063fceb95eba99c0eb0a47493
Chitranshu <chitranshu2411@gmail.com>
2026-08-20 23:22:44 +0530

    Runtime: 3ms ( Beats 96.00% )

---
+++ 1004 -  Max Consecutive Ones III/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int i = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == 0) {
+                zeroCount++;
+            }
+            while (zeroCount > k) {
+                if (nums[i] == 0) {
+                    zeroCount--;
+                }
+                i++;
+            }
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+        return maxLen;
+    }
+}
